c38ce98d61f32e7afeb413390461ba120812d950
wwwongsta <38924529+wwwongsta@users.noreply.github.com>
2019-04-23 13:45:44 -0400

    Revert "card view map"
    This reverts commit 44053c5

--- app/src/main/java/com/example/picar/activities/MapsActivity.java
+++ app/src/main/java/com/example/picar/activities/MapsActivity.java
@@ -279,6 +279,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         locationSearch = (EditText) findViewById(R.id.editText_location);
 
+
         // Prompt the user for permission.
         getLocationPermission();
 
